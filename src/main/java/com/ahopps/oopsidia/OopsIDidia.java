bdd9eb08ec5c21705f12e702a6cfbdfa4bb8c0ef
Andrew Hopps <andrewhopps@gmail.com>
2016-12-05 08:53:56 -0500

    Initial commit

---
+++ src/main/java/com/ahopps/oopsidia/OopsIDidia.java
@@ -0,0 +1,20 @@
+package com.ahopps.oopsidia;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = OopsIDidia.MODID, version = OopsIDidia.VERSION)
+public class OopsIDidia
+{
+    public static final String MODID = "OIDIA";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
